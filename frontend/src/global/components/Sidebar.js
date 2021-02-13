ced128e81053fefa74de7f73277804bba8a1f47a
Tony <tonyou1999@hotmail.com>
2021-02-13 18:56:37 -0500

    feat: finish dashboard page (course page)

--- frontend/src/global/components/Sidebar.js
+++ frontend/src/global/components/Sidebar.js
@@ -1,24 +1,59 @@
 import React from 'react';
 
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
-import CancelIcon from '@material-ui/icons/Cancel';
 import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
 import AttachFileOutlinedIcon from '@material-ui/icons/AttachFileOutlined';
 import DashboardOutlinedIcon from '@material-ui/icons/DashboardOutlined';
-import { Tooltip } from '@material-ui/core';
+import { Tooltip, Avatar, Badge } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+import Tony from '../../assets/Tony.jpeg';
+
+const StyledBadge = withStyles(theme => ({
+  badge: {
+    backgroundColor: '#44b700',
+    color: '#44b700',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      borderRadius: '50%',
+      animation: '$ripple 1.2s infinite ease-in-out',
+      border: '1px solid currentColor',
+      content: '""',
+    },
+  },
+  '@keyframes ripple': {
+    '0%': {
+      transform: 'scale(.8)',
+      opacity: 1,
+    },
+    '100%': {
+      transform: 'scale(2.4)',
+      opacity: 0,
+    },
+  },
+}))(Badge);
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-content">
-        <Tooltip title="cancel" placement="right">
-          <CancelIcon
-            className="sidebar-icons__icon"
-            style={{ marginTop: 5 }}
-          />
-        </Tooltip>
+        <StyledBadge
+          overlap="circle"
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          variant="dot"
+        >
+          <Avatar alt="Tony Ou" src={Tony} style={{ width: 50, height: 50 }} />
+        </StyledBadge>
         <div className="sidebar-icons">
           <Tooltip title="Dashboard" placement="right">
             <DashboardOutlinedIcon className="sidebar-icons__icon" />
--- frontend/src/pages/Dashboard/components/Course.js
+++ frontend/src/pages/Dashboard/components/Course.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Course = () => {
+const Course = ({ name, instructor, image, isPast, index }) => {
   return (
-    <div>
-      <h1>Course</h1>
+    <div
+      className={isPast ? 'course--past' : 'course'}
+      style={index === 0 ? { marginLeft: 0 } : {}}
+    >
+      <img src={image} alt={name} className="course__image" />
+      <div className="course-label">
+        <p className="course__header">{name}</p>
+        <p className="course__subtitle">Prof. {instructor}</p>
+      </div>
     </div>
   );
+};
+
+Course.propTypes = {
+  name: PropTypes.string.isRequired,
+  instructor: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  isPast: PropTypes.bool,
+};
+
+Course.defaultProps = {
+  isPast: false,
 };
 
 export default Course;
