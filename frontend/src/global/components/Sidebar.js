ec31613057df9738cef2cfec33b94f72557f54b9
Tony <tonyou1999@hotmail.com>
2021-02-13 17:08:48 -0500

    feat: dashboard styling adjustments

--- frontend/src/global/components/Sidebar.js
+++ frontend/src/global/components/Sidebar.js
@@ -1,23 +1,47 @@
 import React from 'react';
+
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
 import CancelIcon from '@material-ui/icons/Cancel';
 import SettingsOutlinedIcon from '@material-ui/icons/SettingsOutlined';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import ChatBubbleOutlineOutlinedIcon from '@material-ui/icons/ChatBubbleOutlineOutlined';
 import AttachFileOutlinedIcon from '@material-ui/icons/AttachFileOutlined';
+import DashboardOutlinedIcon from '@material-ui/icons/DashboardOutlined';
+import { Tooltip } from '@material-ui/core';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-content">
-        <CancelIcon className="sidebar-icons__icon" />
+        <Tooltip title="cancel" placement="right">
+          <CancelIcon
+            className="sidebar-icons__icon"
+            style={{ marginTop: 5 }}
+          />
+        </Tooltip>
         <div className="sidebar-icons">
-          <AccountCircleOutlinedIcon className="sidebar-icons__icon" />
-          <CalendarTodayIcon className="sidebar-icons__icon" />
-          <ChatBubbleOutlineOutlinedIcon className="sidebar-icons__icon" />
-          <AttachFileOutlinedIcon className="sidebar-icons__icon" />
+          <Tooltip title="Dashboard" placement="right">
+            <DashboardOutlinedIcon className="sidebar-icons__icon" />
+          </Tooltip>
+          <Tooltip title="Friends" placement="right">
+            <AccountCircleOutlinedIcon className="sidebar-icons__icon" />
+          </Tooltip>
+          <Tooltip title="Messages" placement="right">
+            <ChatBubbleOutlineOutlinedIcon className="sidebar-icons__icon" />
+          </Tooltip>
+          <Tooltip title="Calendar" placement="right">
+            <CalendarTodayIcon className="sidebar-icons__icon" />
+          </Tooltip>
+          <Tooltip title="Files" placement="right">
+            <AttachFileOutlinedIcon className="sidebar-icons__icon" />
+          </Tooltip>
         </div>
-        <SettingsOutlinedIcon className="sidebar-icons__icon" />
+        <Tooltip title="Settings" placement="right">
+          <SettingsOutlinedIcon
+            className="sidebar-icons__icon"
+            style={{ marginBottom: 5 }}
+          />
+        </Tooltip>
       </div>
     </div>
   );
