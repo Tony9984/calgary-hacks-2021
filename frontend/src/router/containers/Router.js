4608a4ec3c0a4057775b691b8f3fde726daa7ce1
Xinrui Li <xinruili@hotmail.com>
2021-02-14 00:07:08 -0500

    Feat: Added landing page

--- frontend/src/router/containers/Router.js
+++ frontend/src/router/containers/Router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Layout from '../../global/containers/Layout';
 
+import Home from '../../pages/Home';
 import Dashboard from '../../pages/Dashboard';
 import Friends from '../../pages/Friends';
 import Profile from '../../pages/Profile';
@@ -10,14 +11,15 @@ import Course from '../../pages/CoursePage';
 const Router = () => {
   return (
     <BrowserRouter>
-      <Layout>
-        <Switch>
-          <Route exact path="/" component={Dashboard} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Layout>
+          <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/friends" component={Friends} />
           <Route exact path="/profile/:name" component={Profile} />
           <Route exact path="/course/:courseName" component={Course} />
-        </Switch>
-      </Layout>
+        </Layout>
+      </Switch>
     </BrowserRouter>
   );
 };
